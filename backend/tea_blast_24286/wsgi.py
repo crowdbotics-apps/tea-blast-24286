a9ddac4c6799210a93d2bf8b945fa66ddfa18ebd
Crowdbotics <team@crowdbotics.com>
2021-02-04 21:41:14 +0000

    Update models

--- backend/tea_blast_24286/wsgi.py
+++ backend/tea_blast_24286/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tea_blast_24286.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tea_blast_24286.settings")
 
 application = get_wsgi_application()
